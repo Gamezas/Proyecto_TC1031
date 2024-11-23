95eb5bd28c552c02c1f543b3fcbfa2ff7805706c
Gamezas <142269176+Gamezas@users.noreply.github.com>
2024-11-22 18:42:24 -0600

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -2,10 +2,11 @@
  *  
  *  Nombre: Alejandro Adrián García Martínez
  *  Matrícula: A01707109
- *  Fecha: 20/septiembre/2024
+ *  Fecha: 11//22/2024
  *  Descripción: Clase para el main del proyecto TC1031, aquí se 
  *  almacenan los valores del "inventario" del usuario los cuales
- *  serán organizados por un algoritmo de ordenamiento en "sort.h"
+ *  serán organizados por un algoritmo de ordenamiento en la
+ *  clase Sort
  * 
 */
 
@@ -34,43 +35,52 @@ class Inventory
         std::string printHB();
 };
 
+//O(1)
 template <class T>
 Inventory<T>::Inventory(List<T> &lista)
 {
     hotbar = lista;
 }
 
+//O(1)
 template <class T>
 Inventory<T>::Inventory(Inventory<T> &other)
 {
     hotbar = other.getHotbar();
 }
 
+//O(1)
 template <class T>
 List<T> Inventory<T>::getHotbar()
 {
     return hotbar;
 }
 
+//O(1)
 template <class T>
 void Inventory<T>::setHotbar(List<T> &other)
 {
     hotbar = other;
 }
 
+//O(n) en todos sus casos se tardará n cantidad de veces dependiendo el tamaño de la lista
 template <class T>
 std::string Inventory<T>::printHB()
 {
+    //O(1)
     std::stringstream aux;
     Nodo<T>* aqui = hotbar.getHead();
     aux << aqui->getNodeInfo(aqui);
     aqui = aqui -> siguiente;
-    if(aqui == hotbar.getHead()) cout << "Crash\n";
+    
+    //O(n) n es la canridad de valores en la lista
     while(aqui != hotbar.getHead())
     {
         aux << aqui->getNodeInfo(aqui);
         aqui = aqui ->siguiente;
     }
+
+    //O(1)
     return aux.str();
 }
 
